3a8cf772ca8d0acbeed8e90d225874188700888c
chuckcwh <chuckcwh@gmail.com>
2016-06-05 15:52:58 +0800

    Update todos on firebase

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -6,15 +6,21 @@ export var setSearchText = (searchText) => {
   return {
     type: 'SET_SEARCH_TEXT',
     searchText
-  }
+  };
+};
+
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  };
 };
 
 export var addTodo = (todo) => {
   return {
     type: 'ADD_TODO',
     todo
-  }
-}
+  };
+};
 
 export var startAddTodo = (text) => {
   return (dispatch, getState) => {
@@ -30,27 +36,36 @@ export var startAddTodo = (text) => {
       dispatch(addTodo({
         ...todo,
         id: todoRef.key
-      }))
-    })
-  }
-}
+      }));
+    });
+  };
+};
 
 export var addTodos = (todos) => {
   return {
     type: 'ADD_TODOS',
     todos
-  }
-}
+  };
+};
 
-export var toggleShowCompleted = () => {
+export var updateTodo = (id, updates) => {
   return {
-    type: 'TOGGLE_SHOW_COMPLETED'
-  }
-}
+    type: 'UPDATE_TODO',
+    id,
+    updates
+  };
+};
 
-export var toggleTodo = (id) => {
-  return {
-    type: 'TOGGLE_TODO',
-    id
-  }
-}
+export var startToggleTodo = (id, completed) => {
+  return (dispatch, getState) => {
+    var todoRef = firebaseRef.child(`todos/${id}`);
+    var updates = {
+      completed,
+      completedAt: completed ? moment().unix() : null
+    };
+
+    return todoRef.update(updates).then(() => {
+      dispatch(updateTodo(id, updates));
+    });
+  };
+};
